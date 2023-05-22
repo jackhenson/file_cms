f7211de373c6e2b1c2e71ac9692b5050fb88faaf
Jack H <jackhensonmedia@gmail.com>
2023-05-21 21:30:55 -0400

    Added method to create/setup documents for minitest test suite

--- cms.rb
+++ cms.rb
@@ -50,7 +50,7 @@ end
 get "/:filename" do
   file_path = File.join(data_path, params['filename']) 
 
-  if File.file?(file_path) 
+  if File.exist?(file_path) 
     load_file_content(file_path) 
   else
     session[:message] = "#{params['filename']} does not exist."
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -18,22 +18,32 @@ class CMSTest < Minitest::Test
   def setup
     FileUtils.mkdir_p(data_path)
   end
-  
+
   def teardown
     FileUtils.rm_rf(data_path)
   end
 
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+
     get "/"
 
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.txt"
   end
 
   def test_viewing_text_document
+    create_document "history.txt", "Ruby 0.95 released"
+
     get "/history.txt"
 
     assert_equal 200, last_response.status
@@ -56,6 +66,8 @@ class CMSTest < Minitest::Test
   end
 
   def test_viewing_markdown_document
+    create_document "about.md", "# About Ruby"
+
     get "/about.md"
 
     assert_equal 200, last_response.status
@@ -64,6 +76,8 @@ class CMSTest < Minitest::Test
   end
 
   def test_editing_document
+    create_document "changes.txt"
+
     get "/changes.txt/edit"
 
     assert_equal 200, last_response.status
@@ -84,4 +98,5 @@ class CMSTest < Minitest::Test
     assert_equal 200, last_response.status
     assert_includes last_response.body, "new content"
   end
+
 end
